c09be910d22b38df3f858d87d29dc751a9b54130
RandyMah <frspinner97@gmail.com>
2017-01-26 15:20:43 +0800

    minor update

--- Websites/WebSite2/testing.aspx.vb
+++ Websites/WebSite2/testing.aspx.vb
@@ -3,11 +3,4 @@ Imports System.Data
 Partial Class testing
     Inherits Page
 
-    Private Sub Calendar1_SelectionChanged(sender As Object, e As EventArgs) Handles Calendar1.SelectionChanged
-        TxtCheckin.Text = Calendar1.SelectedDate
-    End Sub
-
-    Private Sub Calendar2_SelectionChanged(sender As Object, e As EventArgs) Handles Calendar2.SelectionChanged
-        TxtCheckout.Text = Calendar2.SelectedDate
-    End Sub
 End Class
---
+++ Websites/WebSite2/testing2.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class testing2
+    Inherits System.Web.UI.Page
+
+End Class
